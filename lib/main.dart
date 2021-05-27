ea2ce5bdb56e2802f579d164df1515d325f6cf90
Akhele <Moneimb8@gmail.com>
2021-05-27 17:33:51 +0100

    Adding Scaffold : App bar and The body

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Diamond App'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      backgroundColor: Colors.blueGrey,
+      body:
+      Text('test')
+      ,
+    ),
   ));
 }
