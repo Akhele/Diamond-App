942bf1fe2afcf9abe42a568120e40e559138e197
Akhele <Moneimb8@gmail.com>
2021-05-27 17:40:24 +0100

    Adding hot reload functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Diamond App'),
+  runApp(DiamondApp());
+}
+
+class DiamondApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Diamond App'),
+          backgroundColor: Colors.blueGrey,
+        ),
         backgroundColor: Colors.blueGrey,
+        body:
+        Text('tt')
+        ,
       ),
-      backgroundColor: Colors.blueGrey,
-      body:
-      Text('test')
-      ,
-    ),
-  ));
+    );
+  }
 }
